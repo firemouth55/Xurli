330fedb228f2ef907094c320fafd1f0ae526f9d3
Skyler Saville <firemouth55@gmail.com>
2018-10-08 12:07:11 -0600

    Dynamic Footer- hubflex

---
+++ hubflex-footer.js
@@ -0,0 +1,16 @@
+
+var company = "Copyright by " + document.title;
+var hyphen = " - ";
+var copy = "&copy;";
+var copyright = document.getElementsByClassName('extendedfooter');
+var year = (new Date()).getFullYear() + " ";
+
+var xurli = "Provided by <a href='http://xurli.com/'>Xurli</a>";
+var hubflex = "Powered by <a href='http://hubflx.com/'>Hubflx</a> ";
+
+var _comp = copy + year + company + hyphen;
+var _xurli = copy + year + xurli + hyphen;
+var _hubflex = copy + year + hubflex;
+
+
+copyright[0].innerHTML = _comp + _xurli + _hubflex;
